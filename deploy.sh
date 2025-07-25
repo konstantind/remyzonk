82b66139ad6c01ce4a6cec35483e6ece04eef67b
Konstantin D <merty.doc@gmail.com>
2025-07-25 17:10:39 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ cd /root/remyzonk || exit
 git pull origin main
 
 docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --build nginx app db redis
+docker compose -f docker-compose.prod.yml up -d --build nginx app db redis schedule
 
 echo "✅ Деплой завершён"
